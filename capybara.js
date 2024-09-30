7b7058744ea35a6b2109791a664a100d0403f128
kristina Troitski <ktroitski33@gmail.com>
2024-09-30 21:54:17 +0300

    cockroach added

--- capybara.js
+++ capybara.js
@@ -1,3 +1,4 @@
+let currCockroach;
 
 window.onload = function() {
     setGame();
@@ -10,4 +11,25 @@ function setGame() {
         tile.id = i.toString();
         document.getElementById("board").appendChild(tile);
     }
+
+    setInterval(setCocroach, 2000);//2000 mil seconds = 2 seconds
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setCocroach() {
+
+    if(currCockroach){
+       currCockroach.innerHTML ="";
+    }
+   let cocroach = document.createElement("img");
+    cocroach.src = "./cockroach.png";
+
+    let num = getRandomTile();
+    currCockroach = document.getElementById(num);
+    currCockroach.appendChild(cocroach);
+
 }
