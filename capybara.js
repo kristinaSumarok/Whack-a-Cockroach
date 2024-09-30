027bb56bd883119eeafcbb1f3dea6fc973c59788
kristina Troitski <ktroitski33@gmail.com>
2024-09-30 22:26:58 +0300

    game is now playable

--- capybara.js
+++ capybara.js
@@ -1,4 +1,8 @@
 let currCockroach;
+let currCapybaraTile;
+let score = 0;
+let gameOver = false;
+
 
 window.onload = function() {
     setGame();
@@ -9,10 +13,12 @@ function setGame() {
     for(let i=0;i<9;i++){
         let tile = document.createElement("div");
         tile.id = i.toString();
+        tile.addEventListener("click",selectTile);
         document.getElementById("board").appendChild(tile);
     }
 
-    setInterval(setCocroach, 2000);//2000 mil seconds = 2 seconds
+    setInterval(setCocroach, 1000);
+    setInterval(setCapybara, 2000);
 }
 
 function getRandomTile() {
@@ -21,7 +27,9 @@ function getRandomTile() {
 }
 
 function setCocroach() {
-
+    if(gameOver){
+        return;
+    }
     if(currCockroach){
        currCockroach.innerHTML ="";
     }
@@ -29,7 +37,43 @@ function setCocroach() {
     cocroach.src = "./cockroach.png";
 
     let num = getRandomTile();
+
+    if(currCapybaraTile && currCapybaraTile.id === num){
+        return;
+    }
+
     currCockroach = document.getElementById(num);
     currCockroach.appendChild(cocroach);
+}
 
+function setCapybara(){
+    if(gameOver){
+        return;
+    }
+
+    if(currCapybaraTile){
+        currCapybaraTile.innerHTML = "";
+    }
+    let capybara = document.createElement("img");
+    capybara.src = "./capybara.png";
+
+    let num = getRandomTile();
+
+    if(currCockroach && currCockroach.id === num){
+        return;
+    }
+
+    currCapybaraTile = document.getElementById(num);
+    currCapybaraTile.appendChild(capybara);
+}
+
+function selectTile(){
+    if(this == currCockroach){
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    }
+    else if (this == currCapybaraTile){
+        document.getElementById("score").innerText = "Game Over: " + score.toString();
+        gameOver=true;
+    }
 }
